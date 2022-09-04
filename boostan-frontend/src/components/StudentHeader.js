cfc01a0d8b6a7bbeaf59c0c9cdf64df217b4423c
MJSahebnasi <mjsahebnasi@gmail.com>
2022-09-04 22:10:15 +0430

    minor graphical changes

--- boostan-frontend/src/components/StudentHeader.js
+++ boostan-frontend/src/components/StudentHeader.js
@@ -7,10 +7,12 @@ function StudentHeader() {
     return (
         <div className="student_header">
             <div className="student_header_container">
-                <Link className='menu_nutton_link' to={"/StudentPage/register"}><h3>ثبت نام کلاس</h3></Link>
-                <Link className='menu_nutton_link' to={"/StudentPage/my_classes"}><h3>کلاس های من</h3></Link>
-                <Link className='menu_nutton_link' to={"/StudentPage/prev_semesters"}><h3>آمار ترم های قبل</h3></Link>
-                <Link className='menu_nutton_link' to={"/StudentPage/reviews"}><h3>نظرات دانشجویان</h3></Link>
+                <div className='menue_buttons_div'>
+                    <Link className='menu_nutton_link' to={"/StudentPage/register"}><h3>ثبت نام کلاس</h3></Link>
+                    <Link className='menu_nutton_link' to={"/StudentPage/my_classes"}><h3>کلاس های من</h3></Link>
+                    <Link className='menu_nutton_link' to={"/StudentPage/prev_semesters"}><h3>آمار ترم های قبل</h3></Link>
+                    <Link className='menu_nutton_link' to={"/StudentPage/reviews"}><h3>نظرات دانشجویان</h3></Link>
+                </div>
 
                 <div>
                     {/* <Link id='cartLink' to={"/cart"}><button type="button" className="cart_button">Cart</button></Link> */}
