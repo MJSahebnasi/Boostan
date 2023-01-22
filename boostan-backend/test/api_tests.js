58889190419c3d044151cfc6f9a9596420ceef2f
MJSahebnasi <mjsahebnasi@gmail.com>
2023-01-22 22:33:49 +0330

    get my_courses test added

--- boostan-backend/test/api_tests.js
+++ boostan-backend/test/api_tests.js
@@ -9,4 +9,14 @@ describe('get all_courses',()=>{
         expect(response.status).to.equal(200)
         expect(response.body).to.be.an.instanceof(Array);
     })
+});
+
+describe('get my_courses',()=>{
+    it('GET /my_courses/ returns an array of a student\'s registered courses',async()=>{
+        const response= await request(server).get('/all_courses/', '972401');
+        expect(response.status).to.equal(200)
+        expect(response.body).to.be.an.instanceof(Array)
+        expect(response.body).to.have.key('registerars')
+        expect(response.body.registerars).to.contain('972401')
+    })
 })
