2a267f06132e6c4bfb6022e6123e7429639abad5
MJSahebnasi <mjsahebnasi@gmail.com>
2023-01-23 01:37:07 +0330

    post preregister tests added

--- boostan-backend/test/api_tests.js
+++ boostan-backend/test/api_tests.js
@@ -3,7 +3,7 @@ const request = require('supertest')
 var chai = require('chai')
 var expect = chai.expect;
 
-describe('get all_courses',()=>{
+describe('GET all_courses',()=>{
     it('GET /all_courses/ returns an array of courses',async()=>{
         const response= await request(server).get('/all_courses/');
         expect(response.status).to.equal(200)
@@ -11,7 +11,7 @@ describe('get all_courses',()=>{
     })
 });
 
-describe('get my_courses - test #1 - not empty array',()=>{
+describe('GET my_courses - test #1 - not empty array',()=>{
     it('GET /my_courses/ returns an array of a student\'s registered courses',async()=>{
         const response= await request(server).get('/my_courses/972401');
 
@@ -22,13 +22,39 @@ describe('get my_courses - test #1 - not empty array',()=>{
     })
 })
 
-describe('get my_courses - test #2 - empty array',()=>{
+describe('GET my_courses - test #2 - empty array',()=>{
     it('GET /my_courses/ returns an array of a student\'s registered courses',async()=>{
-        const response= await request(server).get('/my_courses/12341451111');
+        const response= await request(server).get('/preregister/12341451111');
 
         expect(response.status).to.equal(200);
         expect(response.body).to.be.an.instanceof(Array);
         expect(response.body).to.be.empty;
+    })
+})
+
+describe('POST preregister - test #1 - successfull registeration',()=>{
+    it('POST /preregister/ returns a boolean: success',async()=>{
+        const payload = {std_id: '972401', course_code: '10002'}
+        const response = await request(server)
+            .post('/preregister')
+            .send(payload)
+
+        expect(response.status).to.equal(200);
+        expect(response.body).to.be.an.instanceof(Boolean);
+        expect(response.body).to.be(true);
+    })
+})
+
+describe('POST preregister - test #2 - fail: already registered',()=>{
+    it('POST /preregister/ returns a boolean: success',async()=>{
+        const payload = {std_id: '972401', course_code: '10001'}
+        const response = await request(server)
+            .post('/preregister')
+            .send(payload)
+
+        expect(response.status).to.equal(200);
+        expect(response.body).to.be.an.instanceof(Boolean);
+        expect(response.body).to.be(false);
     })
 })
 
