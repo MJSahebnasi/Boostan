e6e2465a6cb94e9cc693fa72eaccb0f0b4ccbaf4
MJSahebnasi <mjsahebnasi@gmail.com>
2023-01-22 22:00:12 +0330

    url modified

--- boostan-backend/test/api_tests.js
+++ boostan-backend/test/api_tests.js
@@ -1,6 +1,11 @@
-describe('get all_available_courses',()=>{
-    it('GET /all_available_courses/ returns an array of courses',async()=>{
-        const response= await request(server).get('/all_available_courses/');
+const server = require('../index')
+const request = require('supertest')
+var chai = require('chai')
+var expect = chai.expect;
+
+describe('get all_courses',()=>{
+    it('GET /all_courses/ returns an array of courses',async()=>{
+        const response= await request(server).get('/all_courses/');
         expect(response.status).to.equal(200)
         expect(response.body).to.be.an.instanceof(Array);
     })
